e921853e7e974f9c389db06a7bfc39d2e0897628
Max <maxbuinevich@gmail.com>
2019-07-25 11:25:39 +0300

    Updates

---
+++ precache-manifest.2f331bd6eef503f8ca871617a1d004a1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2635ced96d33e89dd7d67faf7473d870",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "c63931d9c2bb58c14560",
+    "url": "/todolist/static/js/2.d9a056d6.chunk.js"
+  },
+  {
+    "revision": "7cdeb26926f3396c0350",
+    "url": "/todolist/static/js/main.0b5c8672.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  },
+  {
+    "revision": "323097465898a7969446b9cf18ab6194",
+    "url": "/todolist/static/media/home.32309746.svg"
+  },
+  {
+    "revision": "6b7c2156d13e8cd7cd845bd60c2e0d69",
+    "url": "/todolist/static/media/more.6b7c2156.svg"
+  },
+  {
+    "revision": "ef6fb876191ab11b8a6c78e283d952a4",
+    "url": "/todolist/static/media/settings.ef6fb876.svg"
+  },
+  {
+    "revision": "ca8adc9b3141ae9d46d2a77b719bb0ad",
+    "url": "/todolist/static/media/spiderman.ca8adc9b.jpg"
+  },
+  {
+    "revision": "99163dfa770122c0a8415491ba303200",
+    "url": "/todolist/static/media/three-rows-layout-content-structure-design.99163dfa.svg"
+  },
+  {
+    "revision": "dfa44a763b1df039eccf0ce6a1757e16",
+    "url": "/todolist/static/media/to-do-list.dfa44a76.svg"
+  },
+  {
+    "revision": "956b42da2cf69d46eb47812787c44213",
+    "url": "/todolist/static/media/two-rows-and-three-columns-layout.956b42da.svg"
+  }
+]);
